5e814a2d8e6cfece3c9ebe1565a2a2b62948a7db
Carlos ALvarez <carlosmail1978@gmail.com>
2023-05-25 15:44:56 +0200

    Add generalError tests

--- src/middlewares/error/__tests__/gereralError.test.ts
+++ src/middlewares/error/__tests__/gereralError.test.ts
@@ -7,6 +7,14 @@ beforeEach(() => {
 });
 
 describe("Given a general Error middleware", () => {
+  const req = {};
+
+  const res: Pick<Response, "status" | "json"> = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  const next = jest.fn();
   describe("When it receives a 404 'Endpoint not found' error and a response", () => {
     const statusCode = 404;
 
@@ -17,15 +25,6 @@ describe("Given a general Error middleware", () => {
     const expectedErrorMessage = {
       message: json,
     };
-
-    const req = {};
-
-    const res: Pick<Response, "status" | "json"> = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
-
-    const next = jest.fn();
 
     test("Then it should call the response's method status with 404", () => {
       generalError(
@@ -41,6 +40,38 @@ describe("Given a general Error middleware", () => {
     test("Then it should call the response's json with a 'Endpoint not found' message", () => {
       generalError(
         error,
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.json).toHaveBeenCalledWith(expectedErrorMessage);
+    });
+  });
+
+  describe("When it receives an error without status code and a response", () => {
+    const json = "General error";
+
+    const error = new Error(json);
+    test("Then it should call the response's method status with 500", () => {
+      const statusCode = 500;
+      generalError(
+        error as CustomError,
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.status).toHaveBeenCalledWith(statusCode);
+    });
+
+    test("Then it should call the response's method json with a message 'General error'", () => {
+      const expectedErrorMessage = {
+        message: json,
+      };
+
+      generalError(
+        error as CustomError,
         req as Request,
         res as Response,
         next as NextFunction
